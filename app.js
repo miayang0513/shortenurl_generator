c64f18223608add31c29b35e555906a9a3e6c694
miayang0513 <a16168972@gmail.com>
2019-05-23 00:29:50 +0800

    Uninstall mysql and sequelize and Use mongodb to complete project

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-	res.send('gee')
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'pug')
+
+const db = mongoose.connection
+mongoose.set('debug', true)
+mongoose.connect('mongodb://localhost/shortUrl', {
+  useNewUrlParser: true,
+  useCreateIndex: true
 })
 
-app.listen(port, () => {
-	console.log(`App is running on http://localhost:${port}`)
+db.on('error', () => {
+  console.log('mongodb error!')
 })
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.use('/', require('./route'))
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`)
+})
+
---
+++ route.js
@@ -0,0 +1,44 @@
+const exprees = require('express')
+const router = exprees.Router()
+const Url = require('./models/url')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/shorten', (req, res) => {
+  const shortenUrl = sample()
+  Url.findOne({ shortenUrl }).then(url => {
+    if (url) {
+      res.redirect('/shorten')
+    } else {
+      const newUrl = 'http:localhost:3000/' + shortenUrl
+      Url.create({
+        originUrl: req.query.url,
+        shortenUrl,
+        newUrl
+      }).then(() => {
+        res.render('new', { newUrl, originUrl: req.query.url })
+      })
+    }
+  })
+})
+
+router.get('/:shortenUrl', (req, res) => {
+  const shortenUrl = req.params.shortenUrl
+  Url.findOne({ shortenUrl }).then(url => {
+    res.redirect(`${url.originUrl}`)
+  })
+})
+
+module.exports = router
+
+function sample() {
+  const char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  let url = ''
+  for (let i = 0; i < 5; i++) {
+    const randomPick = Math.floor(Math.random() * char.length)
+    url += char[randomPick]
+  }
+  return url
+}
